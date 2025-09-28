c0d780546d5c4702c6069af5d42444180bec475a
santu7212 <santupramanik540@gmail.com>
2025-09-28 20:21:20 +0530

    plugin

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
+ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      external: ["mongoose"], // ✅ put it here, not in plugins
+    },
+  },
 });
